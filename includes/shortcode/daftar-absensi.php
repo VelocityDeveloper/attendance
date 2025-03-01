5f61f96175244d9c0de4916e439aad7de613e036
adityathok <aditya0thok@gmail.com>
2025-03-01 10:47:20 +0700

    ubah daftar absen

--- includes/shortcode/daftar-absensi.php
+++ includes/shortcode/daftar-absensi.php
@@ -39,7 +39,8 @@ function daftar_absensi_shortcode()
         <div class="container mt-4">
           <div class="row bg-dark text-white py-2">
             <div class="col-3">Tanggal</div>
-            <div class="col-3">Jam</div>
+            <div class="col-3">Masuk</div>
+            <div class="col-3">Pulang</div>
             <div class="col">Keterangan</div>
             <div class="col-2"></div>
           </div>
@@ -47,34 +48,81 @@ function daftar_absensi_shortcode()
           <template x-for="absen in absensi" :key="absen.id">
             <div class="row border-bottom py-2">
               <div class="col-3">
-                <span class="text-nowrap" x-text="formatTanggal(absen.time)"></span>
+                <span class="text-nowrap" x-text="formatTanggal(absen.masuk.time)"></span>
               </div>
               <div class="col-3">
-                <span x-text="formatJam(absen.time)"></span>
+                <template x-if="absen.masuk">
+                  <span>
+                    <span x-text="formatJam(absen.masuk.time)"></span>
+                    <template x-if="absen.masuk.status == 'Terlambat'">
+                      <span class="text-danger" x-bind:title="absen.masuk.status">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" fill="currentColor" class="bi bi-exclamation-circle-fill" viewBox="0 0 16 16">
+                          <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"></path>
+                        </svg>
+                      </span>
+                    </template>
+                  </span>
+                </template>
+              </div>
+              <div class="col-3">
+                <template x-if="absen.pulang">
+                  <span>
+                    <span x-text="formatJam(absen.pulang.time)"></span>
+                    <template x-if="absen.pulang.status === 'Pulang awal'">
+                      <span class="text-danger" x-bind:title="absen.pulang.status">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" fill="currentColor" class="bi bi-exclamation-circle-fill" viewBox="0 0 16 16">
+                          <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"></path>
+                        </svg>
+                      </span>
+                    </template>
+                  </span>
+                </template>
               </div>
               <div class="col">
                 <span x-text="absen.status"></span>
                 <span class="text-danger" x-text="absen.selisih_waktu"></span>
               </div>
               <div class="col-2 text-end px-0">
-                <button @click="deleteAbsensi(absen.id)" class="btn btn-danger btn-sm">
-                  <template x-if="singleLoading[absen.id]">
-                    <span class="spinner-border spinner-border-sm" aria-hidden="true"></span>
+                <div class="d-flex justify-content-end">
+                  <template x-if="absen.masuk">
+                    <button @click="deleteAbsensi(absen.masuk.id)" class="btn btn-danger btn-sm">
+                      <template x-if="singleLoading[absen.masuk.id]">
+                        <span class="spinner-border spinner-border-sm" aria-hidden="true"></span>
+                      </template>
+                      <template x-if="!singleLoading[absen.masuk.id]">
+                        <span>
+                          <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
+                            <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5" />
+                          </svg>
+                          Masuk
+                        </span>
+                      </template>
+                    </button>
                   </template>
-                  <template x-if="!singleLoading[absen.id]">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
-                      <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5" />
-                    </svg>
+                  <template x-if="absen.pulang">
+                    <button @click="deleteAbsensi(absen.pulang.id)" class="btn btn-danger btn-sm ms-1">
+                      <template x-if="singleLoading[absen.pulang.id]">
+                        <span class="spinner-border spinner-border-sm" aria-hidden="true"></span>
+                      </template>
+                      <template x-if="!singleLoading[absen.pulang.id]">
+                        <span>
+                          <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
+                            <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5" />
+                          </svg>
+                          Pulang
+                        </span>
+                      </template>
+                    </button>
                   </template>
-                </button>
+                </div>
               </div>
 
               <!-- hanya tampil untuk admin -->
               <?php if (current_user_can('administrator')) : ?>
                 <div class="col-12 text-secondary">
-                  <b>IP:</b> <span x-text="absen.ip_address"></span>
-                  <b>DEVICE:</b> <span x-text="absen.device"></span>
-                  <b>Lokasi Absen:</b> <a target="_blank" :href="`https://maps.google.com/?q=${absen.lat},${absen.lng}`">
+                  <b>IP:</b> <span x-text="absen.masuk.ip_address"></span>
+                  <b>DEVICE:</b> <span x-text="absen.masuk.device"></span>
+                  <b>Lokasi Absen:</b> <a target="_blank" :href="`https://maps.google.com/?q=${absen.masuk.lat},${absen.masuk.lng}`">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-geo" viewBox="0 0 16 16">
                       <path fill-rule="evenodd" d="M8 1a3 3 0 1 0 0 6 3 3 0 0 0 0-6M4 4a4 4 0 1 1 4.5 3.969V13.5a.5.5 0 0 1-1 0V7.97A4 4 0 0 1 4 3.999zm2.493 8.574a.5.5 0 0 1-.411.575c-.712.118-1.28.295-1.655.493a1.3 1.3 0 0 0-.37.265.3.3 0 0 0-.057.09V14l.002.008.016.033a.6.6 0 0 0 .145.15c.165.13.435.27.813.395.751.25 1.82.414 3.024.414s2.273-.163 3.024-.414c.378-.126.648-.265.813-.395a.6.6 0 0 0 .146-.15l.015-.033L12 14v-.004a.3.3 0 0 0-.057-.09 1.3 1.3 0 0 0-.37-.264c-.376-.198-.943-.375-1.655-.493a.5.5 0 1 1 .164-.986c.77.127 1.452.328 1.957.594C12.5 13 13 13.4 13 14c0 .426-.26.752-.544.977-.29.228-.68.413-1.116.558-.878.293-2.059.465-3.34.465s-2.462-.172-3.34-.465c-.436-.145-.826-.33-1.116-.558C3.26 14.752 3 14.426 3 14c0-.599.5-1 .961-1.243.505-.266 1.187-.467 1.957-.594a.5.5 0 0 1 .575.411" />
                     </svg>
@@ -237,8 +285,12 @@ function get_absensi_list_ajax()
     }
   }
 
-  // Proses keterlambatan dan pulang sebelum waktunya
-  foreach ($absensi as &$absen) {
+  //kelompokkan data absensi berdasarkan tanggal
+  $grouped_absensi = [];
+  foreach ($absensi as $absen) {
+    $tanggal = date('Y-m-d', strtotime($absen['time']));
+
+    // Proses keterlambatan dan pulang sebelum waktunya
     if ($user_shift) {
       $absen_time = strtotime($absen['time']);
       $shift_start = strtotime(date('Y-m-d', $absen_time) . ' ' . $user_shift['start']);
@@ -252,15 +304,17 @@ function get_absensi_list_ajax()
         $absen['status'] = ($absen_time < $shift_end) ? 'Pulang awal' : 'Pulang Tepat Waktu';
       }
 
-      $selish_masuk = $absen_time > $shift_start ? $absen_time - $shift_start : '00:00:00';
-      $selish_pulang = $absen_time < $shift_end ? $shift_end - $absen_time : '00:00:00';
+      $selish_masuk = $absen_time > $shift_start ? $absen_time - $shift_start : 0;
+      $selish_pulang = $absen_time < $shift_end ? $shift_end - $absen_time : 0;
       $absen['selisih_waktu'] = ($absen['type'] == 'masuk') ? gmdate('H:i', $selish_masuk) : gmdate('H:i', $selish_pulang);
     } else {
       $absen['status'] = 'Shift Tidak Ditemukan';
     }
+
+    $grouped_absensi[$tanggal][$absen['type']] = $absen;
   }
 
-  wp_send_json_success(['user_id' => $user_id, 'absensi' => $absensi]);
+  wp_send_json_success(['user_id' => $user_id, 'absensi' => $grouped_absensi], 200);
 }
 add_action('wp_ajax_get_absensi_list', 'get_absensi_list_ajax');
 
