5a9849dee7d768dfde597d72145ce404ed5d35aa
adityathok <aditya0thok@gmail.com>
2025-03-01 11:40:41 +0700

    keterangan absen

--- includes/shortcode/daftar-absensi.php
+++ includes/shortcode/daftar-absensi.php
@@ -45,22 +45,38 @@ function daftar_absensi_shortcode()
             <div class="col-2"></div>
           </div>
 
-          <template x-for="absen in absensi" :key="absen.id">
+          <template x-for="absen in absensi">
             <div class="row border-bottom py-2">
               <div class="col-3">
                 <span class="text-nowrap" x-text="formatTanggal(absen.masuk.time)"></span>
               </div>
               <div class="col-3">
                 <template x-if="absen.masuk">
-                  <span>
+                  <span class="d-flex justify-content-start align-items-center">
                     <span x-text="formatJam(absen.masuk.time)"></span>
                     <template x-if="absen.masuk.status == 'Terlambat'">
-                      <span class="text-danger" x-bind:title="absen.masuk.status">
+                      <span class="text-danger mx-1" x-bind:title="absen.masuk.status">
                         <svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" fill="currentColor" class="bi bi-exclamation-circle-fill" viewBox="0 0 16 16">
                           <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"></path>
                         </svg>
                       </span>
                     </template>
+
+                    <?php if (current_user_can('administrator')) : ?>
+                      <button @click="deleteAbsensi(absen.masuk.id)" class="btn btn-link text-danger btn-sm">
+                        <template x-if="singleLoading[absen.masuk.id]">
+                          <span class="spinner-border spinner-border-sm" aria-hidden="true"></span>
+                        </template>
+                        <template x-if="!singleLoading[absen.masuk.id]">
+                          <span>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
+                              <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5" />
+                            </svg>
+                          </span>
+                        </template>
+                      </button>
+                    <?php endif; ?>
+
                   </span>
                 </template>
               </div>
@@ -69,52 +85,42 @@ function daftar_absensi_shortcode()
                   <span>
                     <span x-text="formatJam(absen.pulang.time)"></span>
                     <template x-if="absen.pulang.status === 'Pulang awal'">
-                      <span class="text-danger" x-bind:title="absen.pulang.status">
+                      <span class="text-danger mx-1" x-bind:title="absen.pulang.status">
                         <svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" fill="currentColor" class="bi bi-exclamation-circle-fill" viewBox="0 0 16 16">
                           <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"></path>
                         </svg>
                       </span>
                     </template>
+
+                    <?php if (current_user_can('administrator')) : ?>
+                      <button @click="deleteAbsensi(absen.pulang.id)" class="btn btn-link text-danger btn-sm ms-1">
+                        <template x-if="singleLoading[absen.pulang.id]">
+                          <span class="spinner-border spinner-border-sm" aria-hidden="true"></span>
+                        </template>
+                        <template x-if="!singleLoading[absen.pulang.id]">
+                          <span>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
+                              <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5" />
+                            </svg>
+                          </span>
+                        </template>
+                      </button>
+                    <?php endif; ?>
+
                   </span>
                 </template>
               </div>
-              <div class="col">
-                <span x-text="absen.status"></span>
-                <span class="text-danger" x-text="absen.selisih_waktu"></span>
-              </div>
-              <div class="col-2 text-end px-0">
-                <div class="d-flex justify-content-end">
-                  <template x-if="absen.masuk">
-                    <button @click="deleteAbsensi(absen.masuk.id)" class="btn btn-danger btn-sm">
-                      <template x-if="singleLoading[absen.masuk.id]">
-                        <span class="spinner-border spinner-border-sm" aria-hidden="true"></span>
-                      </template>
-                      <template x-if="!singleLoading[absen.masuk.id]">
-                        <span>
-                          <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
-                            <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5" />
-                          </svg>
-                          Masuk
-                        </span>
-                      </template>
-                    </button>
+              <div class="col-2 text-nowrap px-0">
+                <template x-if="absen.masuk">
+                  <template x-if="absen.masuk.status == 'Terlambat'">
+                    <div class="text-danger small" x-text="'Telat '+absen.masuk.selisih_waktu"></div>
                   </template>
-                  <template x-if="absen.pulang">
-                    <button @click="deleteAbsensi(absen.pulang.id)" class="btn btn-danger btn-sm ms-1">
-                      <template x-if="singleLoading[absen.pulang.id]">
-                        <span class="spinner-border spinner-border-sm" aria-hidden="true"></span>
-                      </template>
-                      <template x-if="!singleLoading[absen.pulang.id]">
-                        <span>
-                          <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
-                            <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5" />
-                          </svg>
-                          Pulang
-                        </span>
-                      </template>
-                    </button>
+                </template>
+                <template x-if="absen.pulang">
+                  <template x-if="absen.pulang.status === 'Pulang awal'">
+                    <div class="text-warning small" x-text="'Kurang '+absen.pulang.selisih_waktu"></div>
                   </template>
-                </div>
+                </template>
               </div>
 
               <!-- hanya tampil untuk admin -->
